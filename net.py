9005234d8b6063b15b3d15e3614f3e485e74d044
xvr <ciprianregus2@yahoo.com>
2021-03-07 14:08:59 +0200

    linear regression results

--- net.py
+++ net.py
@@ -1,16 +1,8 @@
 import torch
 import math
 
-def test(a):
-    a.append("adsda")
 
-x = "xyz"
-test(x)
-print(x)
-
-"""
 class BasicNetwork(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.weights = torch.nn.Parameter(torch.randn())
-"""
